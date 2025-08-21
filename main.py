cddc6504463a22eb64136cb9895e010c2f77cc41
Elmin Gulamov <g.elmn@yandex.ru>
2025-08-22 00:30:13 +0400

    fix main

--- main.py
+++ main.py
@@ -28,29 +28,7 @@ app.add_middleware(
 class RequestData(BaseModel):
     message: str
 
-promp = """
-На основе ТЗ создай тест-кейсы в STRICT MARKDOWN FORMAT.
-
-Шаблон для тест-кейсов:
-### Тест-кейс 1: {название}
-**ID:** TC-001
-**Приоритет:** Высокий
-**Шаги:**
-1. {шаг 1}
-2. {шаг 2}
-**Ожидаемый результат:**
-{результат}
-
-Правила:
-1. Сохрани ВСЕ данные из ТЗ
-2. Строго следуй шаблону
-3. Не добавляй дополнительные тексты
-Представь данные в виде списка с фиксированным выравниванием:
-- Используй символ • для пунктов
-- После двоеточия 20 пробелов для выравнивания
-- Переноси строки с отступом 25 пробелов
-- Максимальная ширина блока: 70 символов
-"""
+promp = os.getenv('promp')
 
 
 @app.post("/ask")
@@ -58,7 +36,7 @@ async def ask_ai(data: RequestData):
     url = "https://api.intelligence.io.solutions/api/v1/chat/completions"
     headers = {
         "Content-Type": "application/json",
-        "Authorization": f"Bearer io-v2-eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJvd25lciI6ImFmNTU5YzA5LWQwYmUtNDQyZi1iNDBjLTg2YmNhYzg4ZmM5YiIsImV4cCI6NDkwODgwNzY5N30.pbm1x9iYRaDijFH2Ehj4dvACqb1gciWVq4jNjm1DzNIVrBohHUNDOqEnu5o-vlO8ortuIoOqCeSL2IHMPgMAxw"
+        "Authorization": f"Bearer {os.getenv('api')}"
     }
     payload = {
         "model": "deepseek-ai/DeepSeek-R1-0528",
