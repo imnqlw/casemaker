5e403f41c181b1e8c5c0f142e8646a305f287bac
Elmin Gulamov <g.elmn@yandex.ru>
2025-08-21 14:23:51 +0400

    t

--- main.py
+++ main.py
@@ -108,6 +108,6 @@ if __name__ == "__main__":
         app,
         host="127.0.0.1",
         port=5000,
-        ssl_keyfile="E:/proect/tcasemaker/ssl/localhost+2-key.pem",
-        ssl_certfile="E:/proect/tcasemaker/ssl/localhost+2.pem"
+        #ssl_keyfile="E:/proect/tcasemaker/ssl/localhost+2-key.pem",
+        #ssl_certfile="E:/proect/tcasemaker/ssl/localhost+2.pem"
     )
